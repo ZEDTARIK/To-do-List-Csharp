5bf67345d7cf1838ff1b3a8b9bdaba92c451154e
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-03-25 15:41:18 +0000

    Small Apps Windows From csharp to do list

---
+++ TodoList/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace TodoList
+{
+    partial class frmToDoList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button btnTask;
+            this.lbltoDoList = new System.Windows.Forms.Label();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listViewTasks = new System.Windows.Forms.ListView();
+            btnTask = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbltoDoList
+            // 
+            this.lbltoDoList.AutoSize = true;
+            this.lbltoDoList.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltoDoList.Location = new System.Drawing.Point(24, 177);
+            this.lbltoDoList.Name = "lbltoDoList";
+            this.lbltoDoList.Size = new System.Drawing.Size(143, 29);
+            this.lbltoDoList.TabIndex = 3;
+            this.lbltoDoList.Text = "To-Do List :";
+            // 
+            // txtTask
+            // 
+            this.txtTask.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTask.Location = new System.Drawing.Point(284, 23);
+            this.txtTask.Multiline = true;
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(289, 41);
+            this.txtTask.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Put Here Your Task : ";
+            // 
+            // btnTask
+            // 
+            btnTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            btnTask.Location = new System.Drawing.Point(284, 83);
+            btnTask.Name = "btnTask";
+            btnTask.Size = new System.Drawing.Size(289, 28);
+            btnTask.TabIndex = 2;
+            btnTask.Text = "Save Task";
+            btnTask.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            btnTask.UseVisualStyleBackColor = true;
+            btnTask.Click += new System.EventHandler(this.btnTask_Click);
+            // 
+            // listViewTasks
+            // 
+            this.listViewTasks.HideSelection = false;
+            this.listViewTasks.Location = new System.Drawing.Point(173, 200);
+            this.listViewTasks.Name = "listViewTasks";
+            this.listViewTasks.Size = new System.Drawing.Size(423, 164);
+            this.listViewTasks.TabIndex = 4;
+            this.listViewTasks.UseCompatibleStateImageBehavior = false;
+            // 
+            // frmToDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 411);
+            this.Controls.Add(this.listViewTasks);
+            this.Controls.Add(btnTask);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.lbltoDoList);
+            this.Name = "frmToDoList";
+            this.Text = "To-Do List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltoDoList;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listViewTasks;
+    }
+}
+
---
+++ TodoList/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace TodoList
+{
+    public partial class frmToDoList : Form
+    {
+        private SqlConnection connection;
+
+        public frmToDoList()
+        {
+            InitializeComponent();
+            connection = new SqlConnection("Data Source=Zouhair\\SQLEXPRESS;Initial Catalog=Tasks;Integrated Security=True; MultipleActiveResultSets=true;");
+            
+            try { 
+                connection.Open(); 
+                //CreateTaskTable();
+                LoadTasks();
+
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }    
+
+        }
+
+        private void btnTask_Click(object sender, EventArgs e)
+        {
+            string name = txtTask.Text.Trim();
+            if (!string.IsNullOrEmpty(name))
+            {
+                string sql = "INSERT INTO tasks (name) VALUES (@name)";
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@name", name);
+                command.ExecuteNonQuery();
+                txtTask.Text = "";
+                LoadTasks();
+            }
+        }
+
+        private void CreateTaskTable()
+        {
+            string sql = "CREATE TABLE dbo.tasks (id INT IDENTITY PRIMARY KEY, name NVARCHAR(100) NOT NULL, completed BIT NOT NULL DEFAULT 0)";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            cmd.ExecuteNonQuery();
+        }
+
+        private void LoadTasks()
+        {
+            listViewTasks.Items.Clear();
+            string sql = "SELECT * FROM tasks";
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(0);
+                string name = reader.GetString(1);
+                bool completed = reader.GetBoolean(2);
+                ListViewItem item = new ListViewItem(name);
+                item.Tag = id;
+                item.Checked = completed;
+                listViewTasks.Items.Add(item);
+            }
+        }
+
+    
+
+        private void buttonDeleteTask_Click(object sender, EventArgs e)
+        {
+            if (listViewTasks.SelectedItems.Count > 0)
+            {
+                int id = (int)listViewTasks.SelectedItems[0].Tag;
+                string sql = "DELETE FROM tasks WHERE id = @id";
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@id", id);
+                command.ExecuteNonQuery();
+                LoadTasks();
+            }
+        }
+
+        private void listViewTasks_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            int id = (int)e.Item.Tag;
+            bool completed = e.Item.Checked;
+            string sql = "UPDATE tasks SET completed = @completed WHERE id = @id";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@completed", completed);
+            command.Parameters.AddWithValue("@id", id);
+            command.ExecuteNonQuery();
+        }
+
+
+    }
+}
